9eea6f460885bfcbfa81a40ddee54c50ca37d525
Asfand0007 <asfand.khanzada0007@gmail.com>
2024-06-24 03:01:13 +0500

    Modified template and views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,15 +4,16 @@ from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
-    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title= models.CharField(max_length=200)
-    text= models.TextField()
-    createdDate= models.DateTimeField(default=timezone.now)
-    publishedDate=models.DateTimeField(blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publishedDate= timezone.now
+        self.published_date = timezone.now()
+        print(f"Publishing date: {self.published_date}")  # Debug statement
         self.save()
-    
+
     def __str__(self):
         return self.title
